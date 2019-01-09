afb4b031ecf09f5f8ee4c67cfb47091b38eb3e1d
glucic <glucic@germanpersonnel.de>
2019-01-09 13:30:19 +0100

    first commit

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("./config");
 var filter = require("./filter.js");
 
 client.on("ready", () => {
